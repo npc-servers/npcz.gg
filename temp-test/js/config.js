ba43e03459619db30994a5cfcdb42e4914518348
Redox <mail@redox.to>
2024-06-22 05:53:37 +0200

    Add temp test

---
+++ temp-test/js/config.js
@@ -0,0 +1,38 @@
+// ignore
+var Config = {};
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+    "Enjoy your stay!",
+
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "temp.jpg";
